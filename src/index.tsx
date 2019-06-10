7adb6b51536d0d23bdff80d6cf4a31dd08bbe78b
jquave <jquave@gmail.com>
2019-06-10 12:36:13 -0500

    Remove unneeded code

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
 import App from './components/App';
 import { createBrowserHistory } from 'history';
 const history = createBrowserHistory();
